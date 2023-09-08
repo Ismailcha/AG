6ecb0a032a0cbfba03905e41d58d847479e3e99a
ImadeddineElalouaji <131325828+ImadeddineElalouaji@users.noreply.github.com>
2023-09-08 12:15:44 +0100

    ajax produits + rows
    successfully

--- resources/views/offers/create.blade.php
+++ resources/views/offers/create.blade.php
@@ -71,6 +71,25 @@
             </div>
 
             <!-- Add Products Section -->
+           
+
+            <!-- Table to display selected products -->
+            <h2>Selected Produits</h2>
+            <table class="table" id="selectedProductTable">
+                <thead>
+                    <tr>
+                        <th>Product Name</th>
+                        <th>Prix Achat</th>
+                        <th>Discount</th>
+                        <th>Qty</th>
+                        <th>Discounted Prix Achat</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- This table will be populated with selected products via JavaScript -->
+                </tbody>
+            </table>
             <h2>Add Products to Offer</h2>
             <!-- You can include code here to select products and associate them with the offer -->
             <div class="form-group">
@@ -83,38 +102,87 @@
                     <tr>
                         <th>Nom De produit</th>
                         <th>Image</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($produits as $produit)
                         <tr>
                             <td>{{ $produit->nom }}</td>
-                            <td><img src="{{ asset('storage/' . $produit->image) }}" alt="Produit Image" width="90">
+                            <td><img src="{{ asset('storage/' . $produit->image) }}" alt="Produit Image" width="90"></td>
+                            <td>
+                                <button class="add-product-btn" data-nom="{{ $produit->nom }}" data-prixAchat="{{ $produit->prixAchat }}">+</button>
                             </td>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
+
+            <!-- Submit button -->
+            <button type="submit" class="btn btn-primary">Create Offer</button>
         </form>
     </div>
-
-
-
 </x-default-layout>
+
 <script>
     $(document).ready(function() {
         // Reference to the product table body
         var productTableBody = $('#productTable tbody');
+        // Reference to the selected product table body
+        var selectedProductTableBody = $('#selectedProductTable tbody');
+
+        // Function to fetch and display all products
+        function displayAllProducts() {
+            // Send an AJAX request to the server to get all products
+            $.ajax({
+                type: 'GET',
+                url: '{{ route('produits.search') }}', // Use the correct route name for fetching all products
+                success: function(response) {
+                    // Clear the table body
+                    productTableBody.empty();
+
+                    var produits = response.produits;
+
+                    if (produits.length > 0) {
+                        produits.forEach(function(product) {
+                            // Display each product
+                            productTableBody.append('<tr><td>' + product.nom +
+                                '</td><td><img src="{{ asset('storage/') }}/' +
+                                product.image +
+                                '" alt="Produit Image" width="90"></td>' +
+                                '<td><button class="add-product-btn" data-nom="' + product.nom + '" data-prixAchat="' + product.prixAchat + '">+</button></td></tr>'
+                            );
+                        });
+                    } else {
+                        // No products found
+                        productTableBody.append(
+                            '<tr><td colspan="3">Aucun produit trouvé </td></tr>');
+                    }
+                },
+                error: function(xhr, status, error) {
+                    console.error(xhr.responseText);
+                }
+            });
+        }
+
+        // Initial display of all products
+        displayAllProducts();
 
         // Listen for keyup event on the search input
         $('#searchInput').keyup(function() {
             // Get the search query value
             var query = $(this).val();
 
-            // Send an AJAX request to the server
+            // If the search query is empty, display all products
+            if (query === '') {
+                displayAllProducts();
+                return;
+            }
+
+            // Send an AJAX request to the server to search for products
             $.ajax({
                 type: 'GET',
-                url: '{{ route('produits.search') }}', // Use the correct route name
+                url: '{{ route('produits.search') }}', // Use the correct route name for searching products
                 data: {
                     query: query
                 },
@@ -131,13 +199,14 @@
                                 productTableBody.append('<tr><td>' + product.nom +
                                     '</td><td><img src="{{ asset('storage/') }}/' +
                                     product.image +
-                                    '" alt="Produit Image" width="90"></td></tr>'
+                                    '" alt="Produit Image" width="90"></td>' +
+                                    '<td><button class="add-product-btn" data-nom="' + product.nom + '" data-prixAchat="' + product.prixAchat + '">+</button></td></tr>'
                                 );
                             });
                         } else {
                             // No matching products found
                             productTableBody.append(
-                                '<tr><td colspan="2">Resultat non trouvé </td></tr>');
+                                '<tr><td colspan="3">Aucun résultat trouvé </td></tr>');
                         }
                     }
                 },
@@ -146,5 +215,30 @@
                 }
             });
         });
+
+        // Listen for click events on the "+" buttons
+        $(document).on('click', '.add-product-btn', function() {
+            var productName = $(this).data('nom');
+            var productPrixAchat = $(this).data('prixAchat');
+
+            // Create a new row for selected product
+            var newRow = '<tr>' +
+                '<td><input type="text" class="form-control" value="' + productName + '" name="productName[]" readonly></td>' +
+                '<td><input type="text" class="form-control" value="' + productPrixAchat + '" name="productPrixAchat[]" readonly></td>' +
+                '<td><input type="number" class="form-control" name="discount[]"></td>' +
+                '<td><input type="number" class="form-control" name="qty[]"></td>' +
+                '<td><input type="number" class="form-control" name="discountedPrixAchat[]"></td>' +
+                '<td><button class="remove-product-btn">-</button></td>' +
+                '</tr>';
+
+            // Append the new row to the selected product table
+            selectedProductTableBody.append(newRow);
+        });
+
+        // Listen for click events on the "-" buttons to remove rows
+        $(document).on('click', '.remove-product-btn', function() {
+            $(this).closest('tr').remove();
+        });
     });
 </script>
+
