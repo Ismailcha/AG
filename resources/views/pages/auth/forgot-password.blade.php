c6fb1638c9d77a64383fb9266c01d1dfe07d06a2
Ismail Chahir <ismailchahir01@gmail.com>
2023-10-07 22:30:26 +0100

    forgot password page

--- resources/views/pages/auth/forgot-password.blade.php
+++ resources/views/pages/auth/forgot-password.blade.php
@@ -1,19 +1,20 @@
 <x-auth-layout>
 
     <!--begin::Form-->
-    <form class="form w-100" novalidate="novalidate" id="kt_password_reset_form" data-kt-redirect-url="{{ route('login') }}" action="{{ route('password.request') }}">
+    <form class="form w-100" novalidate="novalidate" id="kt_password_reset_form" data-kt-redirect-url="{{ route('login') }}"
+        action="{{ route('password.request') }}">
         @csrf
         <!--begin::Heading-->
         <div class="text-center mb-10">
             <!--begin::Title-->
             <h1 class="text-dark fw-bolder mb-3">
-                Forgot Password ?
+                Mot de passe oublié ?
             </h1>
             <!--end::Title-->
 
             <!--begin::Link-->
             <div class="text-gray-500 fw-semibold fs-6">
-                Enter your email to reset your password.
+                Entrez votre e-mail pour réinitialiser votre mot de passe.
             </div>
             <!--end::Link-->
         </div>
@@ -22,17 +23,18 @@
         <!--begin::Input group--->
         <div class="fv-row mb-8">
             <!--begin::Email-->
-            <input type="text" placeholder="Email" name="email" autocomplete="off" class="form-control bg-transparent" value="demo@demo.com"/>
+            <input type="text" placeholder="Email" name="email" autocomplete="off"
+                class="form-control bg-transparent" value="demo@demo.com" />
             <!--end::Email-->
         </div>
 
         <!--begin::Actions-->
         <div class="d-flex flex-wrap justify-content-center pb-lg-0">
             <button type="button" id="kt_password_reset_submit" class="btn btn-primary me-4">
-                @include('partials/general/_button-indicator', ['label' => 'Submit'])
+                @include('partials/general/_button-indicator', ['label' => 'Envoyer'])
             </button>
 
-            <a href="{{ route('login') }}" class="btn btn-light">Cancel</a>
+            <a href="{{ route('login') }}" class="btn btn-light">Annuler</a>
         </div>
         <!--end::Actions-->
     </form>
